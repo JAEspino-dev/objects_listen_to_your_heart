71711223ea6c8ecea269521212eff655f7fed857
Jorge Espino <jorgeespinonmchs@gmail.com>
2026-09-18 23:22:00 -0400

    Initial Listen to Your Heart project

---
+++ js/main.js
@@ -0,0 +1,19 @@
+//Create a stopwatch object that has four properties and three methods
+const stopwatch = {}
+stopwatch.shape = 'Round'
+stopwatch.color = 'Black'
+stopwatch.material = 'Plastic'
+stopwatch.condition = 'New'
+
+stopwatch.start = () => {
+    console.log('start')
+}
+stopwatch.end = () => {
+    console.log('end')
+}
+stopwatch.selfdestruct = () => {
+    console.log(`This ${stopwatch.material} watch has exploded!`)
+}
+
+console.log(stopwatch)
+console.log(stopwatch.shape)
